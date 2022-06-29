5fed83d5c9959e2622e519ff6dd0ca539e4d35d2
Jane Ngugi <ngugijse22@gmail.com>
2022-06-29 05:48:06 -0700

    puts not printf,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main (void){
+int main(void){
 
-	char str[]="\"Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	return (0);

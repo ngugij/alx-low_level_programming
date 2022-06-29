1ba4a854e3ec83d6dffa54c092243a3ae2b870c0
Jane Ngugi <ngugijse22@gmail.com>
2022-06-29 06:34:48 -0700

    printf not puts.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
-#include <stdio.h>                                                                                                                                                      
-                                                                                                                                                                        
-/**                                                                                                                                                                     
+#include <stdio.h>
+
+/**
  * main - entry point
  * use printf to display output
  * Return: Always 0 (Success)
@@ -9,7 +9,7 @@
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }

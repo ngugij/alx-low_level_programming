33dbf3f568f93b458dfb70ae000609f4d05839ea
Jane Ngugi <ngugijse22@gmail.com>
2022-06-30 15:10:14 -0700

    Print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" , strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65);
 	return (1);
+
 
 }

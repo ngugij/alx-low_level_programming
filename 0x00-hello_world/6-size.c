52593d957aace43ee6dff7dde7f8465ea5902e7e
Jane Ngugi <ngugijse22@gmail.com>
2022-06-30 07:56:47 -0700

    Size of a data type.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 
-	printf("Size of a char: %zu byte(s)\n", size of(char));
-	printf("Size of an int:  %zu byte(s)\n", size of(int));
-	printf("Size of a long int: %zu byte(s)\n", size of(long int));
-	printf("Size of a long long int: %zu byte(s)\n", size of(long long int));
-	printf("Size of a float: %zu byte(s)\n", size of(float))
+	printf("Size of a char: %d byte(s)\n", size of(char));
+	printf("Size of an int:  %d byte(s)\n", size of(int));
+	printf("Size of a long int: %d byte(s)\n", size of(long int));
+	printf("Size of a long long int: %d byte(s)\n", size of(long long int));
+	printf("Size of a float: %d byte(s)\n", size of(float))
 	return (0);
 
 }

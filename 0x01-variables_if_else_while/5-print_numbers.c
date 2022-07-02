0741ad3537f8356152afad4f08577f6c5b9e6a16
Jane Ngugi <ngugijse22@gmail.com>
2022-07-01 18:21:57 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry main
+ * print all single numbers between 0 & 10
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}

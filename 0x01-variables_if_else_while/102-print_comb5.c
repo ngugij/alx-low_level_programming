116cf97d01ad630a3f43384905379b6299508da6
Jane Ngugi <ngugijse22@gmail.com>
2022-07-04 15:50:17 -0700

    More combinations

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print combination of numbers
+ * using putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int x;
+	int j;
+	int n;
+	int comma = 44;
+	int space = 32;
+
+	for (j = 48; j <= 57; j++)
+	{
+		for (n = 48; n <= 57; n++)
+		{
+			for  (i = 48; i <= 57; i++)
+			{
+				for (x = 49; x <= 57; x++)
+				{
+					putchar(j);
+					putchar(n);
+					putchar(space);
+					putchar(i);
+					putchar(x);
+					if (n != 56 && j != 57)
+					{
+						putchar(comma);
+						putchar(space);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+

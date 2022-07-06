d37639c13eb40cfeb71c6bb619bac3793d1e7665
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 16:55:49 -0700

    Print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,12 +15,12 @@ void print_to_98(int n)
 	{
 		printf("%d\n", n);
 	}
-	else if (n < 98)
+	if (n < 98)
 	{
 		printf("%d, ", n);
 		n = n + 1;
 	}
-	else
+	if (n > 98)
 	{
 		printf("%d, ", n);
 		n = n - 1;

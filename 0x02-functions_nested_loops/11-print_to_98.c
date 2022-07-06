4b603267882792c135f0b0abbc52da61905b04f3
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 15:51:37 -0700

    Print numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print sequence from given number to 98
+ * @n: number to begin sequence from
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i);
+		_putchar(44);
+		_putchar(32);
+	}
+	_putchar(10);
+
+}

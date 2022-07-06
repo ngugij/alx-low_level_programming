8b9a8e39d7bb6fe0fa0fb00e57a85ac5e666fbb8
Jane Ngugi <ngugijse22@gmail.com>
2022-07-05 21:39:30 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ *
+ * Return: 0 (Success)
+ */
+
+int _abs(int)
+{
+
+	int j;
+
+	abs(j);
+
+	return (0);
+}

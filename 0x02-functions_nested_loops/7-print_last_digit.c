9eee9a840adea66f46354837d9afa7d93f46fcce
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 07:07:12 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,8 +14,10 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = (n % 10);
+	if (n < 0)
 	{
-		_putchar(n % 10 + '0');
-		return (last_digit);
+		n = n * -1
 	}
+	_putchar(n % 10 + '0');
+	return (last_digit);
 }

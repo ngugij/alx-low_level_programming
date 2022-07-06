e96a6f2239814303d334578edbe4e193d02893a9
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 06:40:17 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,7 +14,8 @@ int print_last_digit(int n)
 	int last_digit;
 
 	last_digit = (n % 10);
-	_putchar(37100, last_digit);
-
-	return last_digit;
+	{
+		printf("%d", last_digit);
+		return last_digit;
+	}
 }

c28b935bc60b139541e74812d319f22329df7bbf
Jane Ngugi <ngugijse22@gmail.com>
2022-07-05 18:54:30 -0700

    Is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * Return: 1 if c is lowercase
+ * Return: 0 otherwise
+ */
+
+int _islower(int c)
+{
+
+	return (_putchar(1, &c, 0));
+}

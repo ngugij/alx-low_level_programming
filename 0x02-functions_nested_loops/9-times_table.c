2793d6021239a249772f07866e94f185da7dfd34
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 15:35:12 -0700

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * times_table - return the multiplication table 
+ * times_table - return the multiplication table
  * upto multiples of 9
  *
  * Return: nothing
@@ -17,11 +17,11 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-
-		for (b = 0; b <= 9; b++);
+		for (b = 0; b <= 9; b++)
 		{
+
 			result = a * b;
-			if(b == 0)
+			if (b == 0)
 			{
 				_putchar(result + '0');
 			}

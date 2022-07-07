62da3645f5bf4367c8ab5e31cb23a2b609051b06
Jane Ngugi <ngugijse22@gmail.com>
2022-07-06 18:45:27 -0700

    Sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
- * sum_multiples - prints sum of multiples of 3 and 5
+ * main - prints sum of multiples of 3 and 5
  * below 1024
  *
  * Return: 0 (Success)
  */
 
-int sum_multiples(void)
+int main(void)
 {
 	int a;
-	unsigned long int total1, total2;
-	unsigned long int sum;
+	int total1, total2;
+	int sum;
 
 	total1 = total2 = sum = 0;
 
@@ -28,6 +28,6 @@ int sum_multiples(void)
 		}
 	}
 	sum = total1 + total2;
-	printf("%lu\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }

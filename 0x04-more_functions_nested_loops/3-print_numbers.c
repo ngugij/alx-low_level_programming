b4b9f50602ff53dadb82fa4a0be9f3911a1b35cb
Jane Ngugi <ngugijse22@gmail.com>
2022-07-07 18:07:59 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ * followed by a new line
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	_putchar(48);
+	_putchar(49);
+	_putchar(50);
+	_putchar(51);
+	_putchar(52);
+	_putchar(53);
+	_putchar(54);
+	_putchar(55);
+	_putchar(56);
+	_putchar(57);
+	_putchar(10);
+}

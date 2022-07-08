da3dcf4551beaef771a327801796477ee9424cf6
Jane Ngugi <ngugijse22@gmail.com>
2022-07-07 18:11:37 -0700

    Print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,15 +10,12 @@
 
 void print_numbers(void)
 {
-	_putchar(48);
-	_putchar(49);
-	_putchar(50);
-	_putchar(51);
-	_putchar(52);
-	_putchar(53);
-	_putchar(54);
-	_putchar(55);
-	_putchar(56);
-	_putchar(57);
+
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
 	_putchar(10);
 }

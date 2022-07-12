f78a0836cd8c905052ab970d68424e87bc798dbc
Jane Ngugi <ngugijse22@gmail.com>
2022-07-11 17:56:32 -0700

    Print a string using puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: string to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+
+}

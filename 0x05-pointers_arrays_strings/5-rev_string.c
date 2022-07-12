8dad9f5e0102a2a5e5c8ddd364973fa47c8fe6a5
Jane Ngugi <ngugijse22@gmail.com>
2022-07-11 19:13:23 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,6 +11,17 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	int i;
+	int len;
+	int temp;
+
+	len = strlen(s);
+
+	for (i = 0; i <= len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i -1];
+		s[len - i - 1] = temp;
+	}	
 
 }

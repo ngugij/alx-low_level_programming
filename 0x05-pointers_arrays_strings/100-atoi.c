cb6a0ceeaffce56cd55b6643aa1d7ed373d4a31d
Jane Ngugi <ngugijse22@gmail.com>
2022-07-12 17:18:48 -0700

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -30,7 +30,7 @@ int _atoi(char *s)
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		if (base > INT_MAX /10 ||
-		    (base ++ INT_MAX /10 &&
+		    (base == INT_MAX /10 &&
 		     s[i] - '0' > 7))
 		{
 			if (sign == 1)

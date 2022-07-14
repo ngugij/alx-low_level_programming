6a0d285702521c77be652989d1db86b1787e4854
Jane Ngugi <ngugijse22@gmail.com>
2022-07-14 15:25:09 -0700

    Concatenate two strings using strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings that use at most n bytes from src
+ * @dest: string to be concatenated
+ * @src: string to append
+ * @n: number of bytes to be used from src
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int lenDest = 0;
+
+	while (dest[i])
+	{
+		lenDest++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[i++] = src[i];
+	}
+	return (dest);
+}

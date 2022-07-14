14240577496db7bd60d2f826046b12aeea3e2c20
Jane Ngugi <ngugijse22@gmail.com>
2022-07-14 15:59:42 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,35 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
+	int i = 0;
+	int result = 0;
+
+	while (1)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+		{
+			break;
+		}
+		else if (s1[i] == '\0')
+		{
+			result = s2[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			result = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			i++;
+		}
+	}
 
 	return (0);
 }

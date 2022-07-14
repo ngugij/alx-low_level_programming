f71c73087c0d87e9035086361130d8a3e14e57f4
Jane Ngugi <ngugijse22@gmail.com>
2022-07-14 16:50:32 -0700

    Capitalize all words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,22 +17,23 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 97 && str[i] <= 122))
 		{
-			if (str[i - 1] == ' ' ||
-					str[i - 1] == '\t' ||
-					str[i - 1] == '\n' ||
-					str[i - 1] == ',' ||
-					str[i - 1] == ';' ||
-					str[i - 1] == '.' ||
-					str[i - 1] == '!' ||
-					str[i - 1] == '?' ||
-					str[i - 1] == '"' ||
-					str[i - 1] == '(' ||
-					str[i - 1] == ')' ||
-					str[i - 1] == '{' ||
-					str[i - 1] == '}')
-			{
-				str[i] = str[i] - 32;
-			}
+			i++;
+		}
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+		{
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);

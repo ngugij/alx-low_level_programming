9ef405a0abfc81094493be9aebe735fc00c5fed6
Jane Ngugi <ngugijse22@gmail.com>
2022-07-14 17:35:54 -0700

    Capitalize all words in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 
 	while (str[++i])
 	{
-		while (!(str[i] == '\0' && str[i] >= 97 && str[i] <= 122))
+		while (!(str[i] != '\0' && str[i] >= 97 && str[i] <= 122))
 		{
 			i++;
 		}

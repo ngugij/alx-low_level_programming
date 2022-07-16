edcfa6731aedfa02e2f185dccdd644f3067ac21c
Jane Ngugi <ngugijse22@gmail.com>
2022-07-15 19:45:44 -0700

    Print buffer 10 bytes per line

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer 10 bytes per line
+ * @b: buffer
+ * @size: bytes to be printed
+ *
+ * Return: nothing
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < size; j = j + 10)
+	{
+		printf("%08x: ", j);
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + j) >= size)
+			{
+				printf(" ");
+			}
+			else
+			{
+				printf("%02x", *(b + i + j));
+			}
+			if ((i % 2) != 0 && i != 0)
+				printf(" ");
+		}
+		for (i = 0; i < 10; i++)
+		{
+			if ((i + j) >= size)
+				break;
+			else if (*(b + i + j) >= 31 &&
+				 *(b + i + j) <= 126)
+				printf("%c", *(b + i + j));
+			else
+				printf(".");
+		}
+		if (j >= size)
+		{
+			continue;
+		}
+		printf("\n");
+	}
+	if (size <= 0)
+	{
+		printf("\n");
+	}
+}

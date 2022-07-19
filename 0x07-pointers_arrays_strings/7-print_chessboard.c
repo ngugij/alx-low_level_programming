ae993d60190cae1074d1dac709b832d3983d6651
Jane Ngugi <ngugijse22@gmail.com>
2022-07-18 17:32:55 -0700

    Print the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2D array for the chessboard
+ *
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar(10);
+	}
+}

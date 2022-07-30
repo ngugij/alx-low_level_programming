75bbc09f65707ac8f5aca2bf321571adf8413416
Jane Ngugi <ngugijse22@gmail.com>
2022-07-29 19:41:45 -0700

    Multiply two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,8 +8,8 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 void errors(void);
-int _strlen(char *str);
-int is_digit(char *str);
+int _strlen(char *str);
+int is_digit(char *str);
 
 
 

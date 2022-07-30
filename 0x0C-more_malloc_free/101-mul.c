01610739908baa92d215528c70fc9bea1c9565d3
Jane Ngugi <ngugijse22@gmail.com>
2022-07-29 19:15:45 -0700

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		mul[i] = 0;
-	for (len1 -= 1; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		n = a[len1] - '0';
 		rem = 0;

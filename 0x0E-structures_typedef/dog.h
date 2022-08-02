ef1da32b3dc321c090677a075bda5e4c96752e00
Jane Ngugi <ngugijse22@gmail.com>
2022-08-01 18:22:30 -0700

    Defining new type in structure using typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - structure holding details of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Holds a structure containing details of dogs
+ */
+
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+
+
+
+#endif

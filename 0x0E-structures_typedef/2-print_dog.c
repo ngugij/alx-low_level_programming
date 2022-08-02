acad363fc66b117efe837296a5107171743b04e0
Jane Ngugi <ngugijse22@gmail.com>
2022-08-01 18:53:32 -0700

    Print a structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -28,5 +28,5 @@ void print_dog(struct dog *d)
 		d->age = "(nil)";
 	}
 
-	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }

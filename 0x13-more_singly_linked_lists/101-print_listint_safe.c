1a3c81e6ac3fd7fc329f5e911f725c8a9a699310
Jane Ngugi <ngugijse22@gmail.com>
2022-08-20 12:09:39 -0700

    Print a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,37 @@
 
 
 /**
+ * real - reallocates memory to the nodes in a list
+ * @old: old list
+ * @size: size of new list
+ * @new: node to append to new list
+ *
+ * Return: pointer to new list
+ */
+
+const listint_t **real(const listint_t **old, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t i;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(old);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		newlist[i] = old[i];
+	}
+	newlist[i] = new;
+	free(old);
+	return (newlist);
+}
+
+
+
+/**
  * print_listint_safe - prints a linked list
  * @head: pointer
  *
@@ -12,22 +43,25 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	size_t i, num = 0;
+	const listint_t **old = NULL;
 
-	while (head)
+	while (head != NULL)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		for (i = 0; i < num; i++)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			if (head == old[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(old);
+				return (num);
+			}
 		}
+		num++;
+		old = real(old, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-
+	free(old);
 	return (num);
 }

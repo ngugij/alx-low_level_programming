e58b8226febbe132c88bbce03541fd456bcf2f52
Jane Ngugi <ngugijse22@gmail.com>
2022-08-30 19:02:27 -0700

    Singly linked list task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer
+ *
+ * Return: nothing
+ */
+
+
+void free_list(list_t *head)
+{
+	list_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+
+}

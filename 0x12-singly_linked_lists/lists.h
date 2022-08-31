50cb067055079d9c2188103a327921377c690a68
Jane Ngugi <ngugijse22@gmail.com>
2022-08-30 19:40:26 -0700

    Singly linked list task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,4 +26,7 @@ size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *str);
+
+
 #endif

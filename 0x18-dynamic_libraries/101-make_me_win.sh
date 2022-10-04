b84adb1b9ae0428b12c1293dc5ddacd9aeb315e4
ngugij <ngugijse22@gmail.com>
2022-10-03 17:47:26 -0700

    Win Jackpot!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wextra -pedantic -fPIC win.c -shared -o mmwin.so
-LD_PRELOAD=/$PWD/mmwin.so
+gcc -Wall -Wextra -pedantic -fPIC win.c -shared -o libwin.so
+LD_PRELOAD=/$PWD/libwin.so

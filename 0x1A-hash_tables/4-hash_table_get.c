8b606125ca0bfae0ef1f916bbca11d6eccf25cb9
ngugij <ngugijse22@gmail.com>
2022-10-23 16:16:06 -0700

    Retrieve a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table you want to look into
+ * @key: key you are looking for
+ *
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (!ht || !key || strcmp(key, "") == 0)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	if (!ht->array[index])
+		return (NULL);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
